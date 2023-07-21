999a662c0b16215e22a1aaaea886481524a9a5b8
Art44 <89154901+art44perov@users.noreply.github.com>
2023-07-21 18:18:31 +0300

    nasa_api
    Connecting to NASA services using the API and receiving data on asteroids that will approach the Earth in the next two days. Dates and periods can be adjusted.

---
+++ nasa_api.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+import requests
+import json
+from googletrans import Translator
+
+def main():
+    translator = Translator()
+    api_key = "API_KEY"
+    start_date = datetime.today()
+    end_date = datetime.date(start_date - timedelta(days=1))
+    start_date = datetime.date(start_date)
+    str_query = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={api_key}"
+    response = requests.get(str_query)
+    if response.status_code == 200:
+        stroke_s = json.loads(response.text)['near_earth_objects']#['2023-07-20']
+        print(f"Астероиды, которые в период с {end_date} по {start_date} будут сближаться с планетой Земля, по данным NASA")
+        print(type(stroke_s))
+        for j in stroke_s:
+            stroke_a = stroke_s[j]
+
+            print(f'\nАстроиды на дату {j}:\n')
+            for i in range(len(stroke_a)):
+                name_a = stroke_a[i]['name']
+                id_a  = stroke_a[i]['id']
+                orbitig = stroke_a[i]['close_approach_data']
+                dangers = stroke_a[i]['is_potentially_hazardous_asteroid']
+                dataNasa = stroke_a[i]['nasa_jpl_url']
+                if dangers:
+                    dangers = "Да"
+                else:
+                    dangers = "Нет"
+
+                if len(orbitig) == 1:
+                    orbitig_a = orbitig[0]['orbiting_body']
+                    orbitig_a = translator.translate(orbitig_a, src='en', dest='ru')
+                else:
+                    orbitig_a = 'None'
+                print(f'\n*ID астероида: {id_a}:\n -Имя астероида: {name_a}\n -Орбитальное тело: {orbitig_a.text}\n -Потеницально опасный объект: {dangers}\n -Больше данных о данном космическом объекте на сайте NASA: {dataNasa}')
+                i=i+1
+    else:
+        print("No connection...")
+
